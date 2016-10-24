4ffc773f7fcaaec53ed87896acd318d394cb145c
fortune ikechukwu ekeruo <legalmody@gmail.com>
2016-10-24 12:34:13 +0100

    Add a test suite for the isQuestion method

--- spec/stringClassExtTest.js
+++ spec/stringClassExtTest.js
@@ -15,7 +15,7 @@ describe('String class extention', function () {
   });
 
 
-  /** Test suite to ensure that toUpper method returns
+  /** Test suite to ensure the toUpper method returns
    * capitalized characters of the string
    */
   describe('toUpper method', function () {
@@ -24,6 +24,19 @@ describe('String class extention', function () {
       expect('fortune'.toUpper()).not.toBe(null);
       expect(typeof 'fortune'.toUpper()).toBe('string');
       expect('fortune'.toUpper()).toEqual('FORTUNE');
+    });
+  });
+
+  /** Test suite to ensure the isQuestion method returns a
+   * boolean if a string is a question(ie. it ends with a question mark)
+   */
+  describe('isQuestion method', function () {
+
+    it('Should return a boolean if a string is a question or not', function () {
+      expect('how are you?'.isQuestion()).not.toBe(null);
+      expect(typeof 'how are you?'.isQuestion()).toBe('boolean');
+      expect('how are you?'.isQuestion()).toEqual(true);
+      expect('how are you'.isQuestion()).toEqual(false);
     });
   });
 
