36d9afd18fd0dac84f173fdaee2f3c52c114b0a8
fortune ikechukwu ekeruo <legalmody@gmail.com>
2016-11-01 20:25:12 +0100

    Add an expectation to the toUpper method test suite

--- spec/stringClassExtTest.js
+++ spec/stringClassExtTest.js
@@ -8,25 +8,33 @@ describe('String class extention', function () {
   describe('hasVowels method', function () {
 
     it('Should return a boolean if a given string has a vowel', function () {
+
       expect('aeiou'.hasVowels()).not.toBe(null);
       expect(typeof 'aeiou'.hasVowels()).toBe('boolean');
       expect('aeiou'.hasVowels()).toBe(true);
       expect('rlhz'.hasVowels()).toBe(false);
       expect('Schrz'.hasVowels()).toBe(false);
+
     });
+
   });
 
 
-  /** Test suite to ensure the toUpper method returns
+  /** 
+   * Test suite to ensure the toUpper method returns
    * capitalized characters of the string
    */
   describe('toUpper method', function () {
 
     it('Should capitalize all characters in a string', function () {
+
       expect('fortune'.toUpper()).not.toBe(null);
       expect(typeof 'fortune'.toUpper()).toBe('string');
       expect('fortune'.toUpper()).toEqual('FORTUNE');
+      expect('fortune'.toUpper()).not.toEqual('fortuNE');
+
     });
+    
   });
 
   /** 
